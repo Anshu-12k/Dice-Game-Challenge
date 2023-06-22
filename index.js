a22f72411bd543f8f2174d51b7e9254548a34b6a
Anshu-12k <96194384+Anshu-12k@users.noreply.github.com>
2023-06-22 16:51:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 var r1=Math.random();
 r1=r1*6;
 r1=Math.floor(r1)+1; //random number between 1-6
-var s1="images/dice"+r1+".png";
+var s1="dice"+r1+".png";
 var image1=document.querySelectorAll("img")[2];
 image1.setAttribute("src",s1);
 
 var r2=Math.floor(Math.random()*6)+1;
-var s2="images/dice"+r2+".png";
+var s2="dice"+r2+".png";
 document.querySelectorAll("img")[3].setAttribute("src",s2);
 
-var im="images/excited.gif";
+var im="excited.gif";
 if(r1>r2)
 {
     document.querySelector("h1").textContent="Player 1 Wins";
